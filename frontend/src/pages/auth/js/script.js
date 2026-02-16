feb82600548a9eb2a88d3349d450fe5712ac8a12
Safadini <igorsabadini157@gmail.com>
2026-02-16 03:53:18 -0300

    feat: login validation

--- frontend/src/pages/auth/js/script.js
+++ frontend/src/pages/auth/js/script.js
@@ -1,13 +1,24 @@
 const submit = document.getElementById("submit");
 
-const API_URL = "http:192.168.56.1:3000";
+const API_URL = "http://192.168.0.150:3000";
 
-const access = async () => {
-    await fetch(`${API_URL}/login`), {
-        method: 'POST'
+const acess = async () => {
+    const user = document.getElementById('user').value;
+    const password = document.getElementById('password').value;
+    const status = document.getElementById('status');
+
+    const response = await fetch(`${API_URL}/auth`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ user, password }) // Envia os dados de verdade
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        // O redirecionamento acontece aqui no cliente!
+        window.location.href = data.redirectUrl;
+    } else {
+        status.innerText = data.mensagem;
     }
-}
-
-const mudarCor = () => {
-    document.querySelector(".enter>input").style.backgroundColor = "#FF0000";
 }
